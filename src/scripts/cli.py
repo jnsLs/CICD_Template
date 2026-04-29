5dde92c4a2d5a67409cc9d7ca598d7b82155c1d1
Jonas <jonas.lederer@tu-berlin.de>
2026-04-29 15:40:15 +0200

    fixed formatting

--- src/scripts/cli.py
+++ src/scripts/cli.py
@@ -1,10 +1,10 @@
 from cowpy.cow import Cowacter
 
+
 def main():
     message = Cowacter().milk("Hello Pixi fans!")
     print(message)
 
 
 if __name__ == "__main__":
-    main()
-
+    main()  # bad comment
